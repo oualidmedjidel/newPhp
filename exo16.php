29a64440d0286c0fb66dca29c18f7c9f54523ce3
Oualid <medjidel.oualid@gmail.com>
2023-05-09 15:15:25 +0200

    exo16

---
+++ exo16.php
@@ -0,0 +1,12 @@
+<?php
+function affiche($n)
+{
+    for ($i = 1; $i <= $n; $i++) {
+        $ligne = '';
+        for ($j = 1; $j <= $i; $j++) {
+            $ligne .= $i;
+        }
+        echo $ligne . "\n <br>";
+    }
+}
+affiche(10);
