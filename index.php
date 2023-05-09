629ba8cc0d8521a98562c759ee5ed76f2a882d6c
Oualid <medjidel.oualid@gmail.com>
2023-05-09 10:35:58 +0200

    film

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-echo "hello word! <br><br></br>"; 
+echo "hello word! <br><br></br>";
 
 
 
+$films = array("Le Parrain", "Forrest Gump", "Le Seigneur des anneaux", "Star Wars", "Matrix");
+
+for ($i = 0; $i < count($films); $i++) {
+    echo $films[$i] . "<br><br>";
+}
+?>
